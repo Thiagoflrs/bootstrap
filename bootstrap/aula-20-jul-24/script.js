4147b178772fb8e458424054799e7e68d7fe6429
Thiago Rodrigues <thiagoflr1@gmail.com>
2024-07-20 12:09:43 -0300

    aula dia 20 jul

---
+++ bootstrap/aula-20-jul-24/script.js
@@ -0,0 +1,10 @@
+function add() {
+    let inputProduto = document.getElementById("inputProduto").value
+    let inputMarca = document.getElementById("inputMarca").value
+    let inputEstoque = document.getElementById("inputEstoque").value
+    let inputCusto = document.getElementById("inputCusto").value
+    let inputVenda = document.getElementById("inputVenda").value
+    
+    console.log(inputProduto, inputMarca, inputEstoque, inputCusto, inputVenda);
+}
+
